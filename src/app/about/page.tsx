3b7afae0521a23ed3c1e622aff0db97ed267672e
yaoandy107 <yaoandy107@gmail.com>
2023-06-25 16:16:00 +0800

    feat: add shadcn/ui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from './about.module.css'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-foreground text-center m-0">About us</h1>
+    </div>
+  )
+}
