741d817a9fd0ff38556da9fb75001ed3416d532b
yaoandy107 <yaoandy107@gmail.com>
2023-07-04 21:42:36 +0800

    fix: refine ui

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -8,7 +8,7 @@ import ImageCompare from "@/components/sample/image_compare";
 export default function Sample() {
   return (
     <div className="mt-[3rem] mb-10 container">
-      <h1 className="mt-4 mb-16 text-center text-4xl font-semibold">Sample</h1>
+      <h1 className="mt-4 mb-16 text-center text-5xl font-semibold">Sample</h1>
       <div className="flex flex-col gap-10">
         <ImageCompare
           title="Columbia University Summer Pre-College Essay Prompt 2023"
--- src/components/sample/image_compare.tsx
+++ src/components/sample/image_compare.tsx
@@ -2,41 +2,38 @@
 import { ImgComparisonSlider } from "@img-comparison-slider/react";
 import Image, { StaticImageData } from "next/image";
 
-const ImageCompare = ({
-  title,
-  description,
-  beforeImage,
-  afterImage,
-}: {
+interface ImageCompareProps {
   title: string;
-  description: string;
   beforeImage: StaticImageData;
   afterImage: StaticImageData;
-}) => {
+}
+
+const ImageCompare = ({
+  title,
+  beforeImage,
+  afterImage,
+}: ImageCompareProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-[600px] mx-auto">
       <h2 className="text-3xl font-semibold self-start mb-3">{title}</h2>
-      <p className="self-start mb-7">{description}</p>
-      <div className="border-2 border-slate-100	overflow-y-scroll h-[450px]">
-        <ImgComparisonSlider>
-          <Image
-            slot="first"
-            src={beforeImage}
-            alt="Essay before revision"
-            width={650}
-            height={800}
-            quality={100}
-          />
-          <Image
-            slot="second"
-            src={afterImage}
-            alt="Essay after revision"
-            width={650}
-            height={800}
-            quality={100}
-          />
-        </ImgComparisonSlider>
-      </div>
+      <ImgComparisonSlider className="border-2 border-slate-100 h-[600px]">
+        <Image
+          slot="first"
+          src={beforeImage}
+          alt="Essay before revision"
+          width={650}
+          height={800}
+          quality={100}
+        />
+        <Image
+          slot="second"
+          src={afterImage}
+          alt="Essay after revision"
+          width={650}
+          height={800}
+          quality={100}
+        />
+      </ImgComparisonSlider>
     </div>
   );
 };
