5b8169d482b1469b58d08a7b68c0609b81baec8f
yaoandy107 <yaoandy107@gmail.com>
2023-07-09 20:08:04 +0800

    fix: add mailto for email in contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,12 @@
 import Calendly from "@/components/contact/calendly";
+import Link from "next/link";
 
 export default function Contact() {
 
   return (
     <div className="container flex flex-col items-center gap-3 mt-10 px-8">
       <h1 className="text-5xl font-semibold">Contact Us</h1>
-      <p>alexander@uwtwriting.com</p>
+      <a href="mailto:alexander@uwtwriting.com?subject=Essay Tutorial Inquiry" className="text-secondary">alexander@uwtwriting.com</a>
       <div className="my-8">
         <Calendly />
       </div>
