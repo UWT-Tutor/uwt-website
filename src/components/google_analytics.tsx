01a2bf4b42d8448c40f52bef2ad48a41f6eb84cd
simple1017 <simple01281017@gmail.com>
2023-07-17 19:25:50 +0800

    feat: add pageview for SPA

--- src/components/google_analytics.tsx
+++ src/components/google_analytics.tsx
@@ -1,12 +1,21 @@
 "use client";
-
+import { pageview } from "@/lib/gtag";
+import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
+import { useEffect } from "react";
 
 export default function GoogleAnalytics({
   GA_TRACKING_ID,
 }: {
   GA_TRACKING_ID: string;
 }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+    pageview(GA_TRACKING_ID, url);
+  }, [pathname, searchParams, GA_TRACKING_ID]);
   return (
     <>
       <Script
