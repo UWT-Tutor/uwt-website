04372fd0bab243b016d1c1b785462d8dc4b0916c
yaoandy107 <yaoandy107@gmail.com>
2023-08-06 12:40:42 +0800

    Remove redundant title in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,5 @@
 import Introduction from "@/components/home/introduction";
 import TopBanner from "@/components/home/top_banner";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Home",
-};
 
 export default function Home() {
   return (
